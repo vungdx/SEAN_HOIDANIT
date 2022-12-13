9e2a6b6a26d158837672881ebceca4dcc1fd5a54
vungdx@thefirstone.jp <vungdx@thefirstone.jp>
2022-12-13 23:15:26 +0700

    init frontend

---
+++ frontend/src/containers/Auth/Login.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import * as actions from "../../store/actions";
+import "./Login.scss";
+import { handleLoginApi } from "../../services/userService";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.btnLogin = React.createRef();
+    this.state = {
+      username: "",
+      password: "",
+      isShowPassword: false,
+    };
+  }
+
+  handleOnChangeUsername = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  };
+
+  handleOnChangePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleLogin = async () => {
+    try {
+      const data = await handleLoginApi(
+        this.state.username,
+        this.state.password
+      );
+      if (data && data.errCode !== 0) {
+        // this.setState({
+        //   errMessage: data.message,
+        // });
+        toast(`${data.message}`);
+      }
+      if (data && data.errCode === 0) {
+        this.props.userLoginSuccess(data.user);
+      }
+    } catch (error) {
+      if (error.response) {
+        if (error.response.data) {
+          toast.error(`${error.response.data.message}`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          // this.setState({
+          //   messageError: error.response.data.message,
+          // });
+        }
+      }
+    }
+  };
+
+  handleShowHidePassword = () => {
+    this.setState({
+      isShowPassword: !this.state.isShowPassword,
+    });
+  };
+
+  render() {
+    return (
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content row">
+            <div className="col-12 login-text">Login</div>
+            <div className="col-12 form-group login-input">
+              <label>Username: </label>
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.username}
+                onChange={(e) => this.handleOnChangeUsername(e)}
+              />
+            </div>
+            <div className="col-12 form-group login-input">
+              <label>Password: </label>
+              <div className="custom-input-password">
+                <input
+                  type={this.state.isShowPassword ? "text" : "password"}
+                  className="form-control"
+                  placeholder="Enter your password"
+                  onChange={(e) => this.handleOnChangePassword(e)}
+                />
+                <span onClick={() => this.handleShowHidePassword()}>
+                  <i
+                    className={
+                      this.state.isShowPassword
+                        ? "far fa-eye"
+                        : "far fa-eye-slash"
+                    }
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="col-12">
+              <button className="btn-login" onClick={() => this.handleLogin()}>
+                Login
+              </button>
+            </div>
+            <div className="col-12">
+              <span className="forgot-password">Forgot your Password?</span>
+            </div>
+            <div className="col-12 text-center">
+              <span className="text-other-login">Or Login with:</span>
+            </div>
+            <div className="col-12 social-login">
+              <i className="fab fa-google-plus-g google"></i>
+              <i className="fab fa-facebook-f facebook"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (path) => dispatch(push(path)),
+
+    // userLoginFail: () => dispatch(actions.userLoginFail()),
+    userLoginSuccess: (userInfo) =>
+      dispatch(actions.userLoginSuccess(userInfo)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);

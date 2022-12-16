56ea37c9f86a50fef085653281af7a0a763de545
Vung <vungdoxuankthd@gmail.com>
2022-12-16 12:03:21 +0700

    #49 Video

--- frontend/src/containers/HomePage/Section/Specialty.js
+++ frontend/src/containers/HomePage/Section/Specialty.js
@@ -4,14 +4,20 @@ import "./Specialty.scss";
 import { changeLanguageApp } from "../../../store/actions/appActions";
 //import css files
 import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import SpecialtyImg1 from '../../../assets/images/slider1.jpg';
+import SpecialtyImg2 from '../../../assets/images/slider2.jpg';
+import SpecialtyImg3 from '../../../assets/images/slider3.jpg';
+import SpecialtyImg4 from '../../../assets/images/slider4.jpg';
+import SpecialtyImg5 from '../../../assets/images/slider5.jpg';
+import SpecialtyImg6 from '../../../assets/images/slider6.jpg';
+import SpecialtyImg7 from '../../../assets/images/slider7.jpg';
+import SpecialtyImg8 from '../../../assets/images/slider8.jpg';
 class Specialty extends Component {
   changeLanguage = (language) => {
     this.props.changeLanguageAppRedux(language)
   }
-
 
   render() {
     const { language } = this.props
@@ -19,21 +25,57 @@ class Specialty extends Component {
       dots: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 4,
       slidesToScroll: 1
     }
     return (
-      <Fragment>
-        <div className="section-specialty">
-          <div className="specialty-content">
-            <Slider {...settings}>
-              <div className="img-customize">
-                <h3>3</h3>
-              </div>
-            </Slider>
+      <div className="section-specialty">
+        <div className="specialty-container">
+          <div className="specialty-header">
+            <span className="title-section">Chuyên khoa phổ biến</span>
+            <button className="btn-section">Xem thêm</button>
+          </div>
+          <div className="specialty-body">
+            <div className="specialty-content">
+              <Slider {...settings}>
+                <div className="img-customize">
+                  <img src={SpecialtyImg1} alt='' />
+                  <div>Tư vấn, trị liệu Tâm lý từ xa</div>
+                </div>
+                <div className="img-customize">
+                  <img src={SpecialtyImg2} alt='' />
+                  <div>Sức khỏe tâm thần từ xa</div>
+                </div>
+                <div className="img-customize">
+                  <img src={SpecialtyImg3} alt='' />
+                  <div>Bác sĩ Da liễu từ xa</div>
+                </div>
+                <div className="img-customize">
+                  <img src={SpecialtyImg4} alt='' />
+                  <div>Bác sĩ Cơ-Xương-Khớp từ xa</div>
+                </div>
+                <div className="img-customize">
+                  <img src={SpecialtyImg5} alt='' />
+                  <div>Bác sĩ Cột sống từ xa</div>
+                </div>
+                <div className="img-customize">
+                  <img src={SpecialtyImg6} alt='' />
+                  <div>Bác sĩ Tiêu hóa từ xa</div>
+                </div>
+                <div className="img-customize">
+                  <img src={SpecialtyImg7} alt='' />
+                  <div>Bác sĩ Nội khoa từ xa</div>
+                </div>
+                <div className="img-customize">
+                  <img src={SpecialtyImg8} alt='' />
+                  <div>Bác sĩ Nam học từ xa</div>
+                </div>
+              </Slider>
+            </div>
           </div>
         </div>
-      </Fragment>
+      </div>
+
     );
   }
 }
@@ -47,7 +89,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    changeLanguageAppRedux: (language)=> dispatch(changeLanguageApp(language))
+    changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
   };
 };
 

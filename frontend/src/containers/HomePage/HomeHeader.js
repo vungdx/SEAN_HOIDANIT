4bfa967c48f41ec08520bb09b134fb4985a62d27
Vung <vungdoxuankthd@gmail.com>
2022-12-16 10:08:01 +0700

    #47 Video

--- frontend/src/containers/HomePage/HomeHeader.js
+++ frontend/src/containers/HomePage/HomeHeader.js
@@ -2,9 +2,20 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
 import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from '../../utils/constant'
+import { changeLanguageApp } from "../../store/actions/appActions";
+
 
 class HomeHeader extends Component {
+  changeLanguage = (language) => {
+    console.log('nhận trong hàm gửi lên', language)
+    this.props.changeLanguageAppRedux(language)
+  }
+
+
   render() {
+    const { language } = this.props
+    console.log("language trond dây", language)
     return (
       <Fragment>
         <div className="home-header-container">
@@ -47,8 +58,8 @@ class HomeHeader extends Component {
               <div className="support">
                 <i className="fas fa-question-circle"></i> Hỗ trợ
               </div>
-              <div className="language-vi">VN</div>
-              <div className="language-en">EN</div>
+              <div className={language === LANGUAGES.VI ? 'language-vi active': 'language-vi'}><span onClick={()=>this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+              <div className={language === LANGUAGES.EN ? 'language-en active': 'language-en'}><span onClick={()=>this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
             </div>
           </div>
         </div>
@@ -113,12 +124,14 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
-    lang: state.app.language,
+    language: state.app.language,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageAppRedux: (language)=> dispatch(changeLanguageApp(language))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
--- frontend/src/store/actions/appActions.js
+++ frontend/src/store/actions/appActions.js
@@ -8,3 +8,8 @@ export const setContentOfConfirmModal = (contentOfConfirmModal) => ({
     type: actionTypes.SET_CONTENT_OF_CONFIRM_MODAL,
     contentOfConfirmModal: contentOfConfirmModal
 });
+
+export const changeLanguageApp = (lang) => ({
+    type: actionTypes.CHANGE_LANGUAGE, 
+    language: lang
+})
